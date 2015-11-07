f742e3a35f5998ca84212f8cc103c58eff621f18
etisab <ariasabeti@gmail.com>
2015-11-07 09:27:10 -0800

    Several improvements and corrections to tests:
    - No longer renaming curbside_challenge namespace
    - Use of patching over mocking to avoid polluting test env
    - Preference to patching with decorator, removing setup methods
    
    Future:
    
    - tests which have not already been parametrized can probably be refactored

--- test_curbside_challenge.py
+++ test_curbside_challenge.py
@@ -1,6 +1,13 @@
 import pytest
 import mock
-import curb_challenge as cc
+
+import curbside_challenge
+from curbside_challenge import dict_keys_to_lower
+from curbside_challenge import listify
+from curbside_challenge import curb_api
+from curbside_challenge import session_generator
+from curbside_challenge import get_response
+from curbside_challenge import get_secret
 
 #helpers
 @pytest.mark.parametrize("input,expected", [
@@ -13,7 +20,7 @@ import curb_challenge as cc
   ),
 ])
 def test_dict_keys_to_lower(input, expected):
-  assert cc.dict_keys_to_lower(input) == expected
+  assert dict_keys_to_lower(input) == expected
 
 @pytest.mark.parametrize("input,expected", [
   (None, [None]),
@@ -23,52 +30,53 @@ def test_dict_keys_to_lower(input, expected):
   ({}, [{}]),
 ])
 def test_listify(input, expected):
-  assert cc.listify(input) == expected
+  assert listify(input) == expected
+
 
 class TestCurbApi:
-  def setup(self):
-    response = mock.Mock()
-    cc.requests.get = mock.Mock(return_value = response)
-
-  def test_endpoint(self):
-    cc.curb_api('any_endpoint')
-    cc.requests.get.assert_called_once_with(
+  @mock.patch('curbside_challenge.requests')
+  def test_endpoint(self, requests_):
+    curb_api('any_endpoint')
+    curbside_challenge.requests.get.assert_called_once_with(
       'http://challenge.shopcurbside.com/any_endpoint',
       headers = {}
     )
 
-  def test_endpoint_headers(self):
+  @mock.patch('curbside_challenge.requests')
+  def test_endpoint_headers(self, requests_mock):
     headers = {'header_name': 'header_value'}
-    cc.curb_api('any_endpoint', headers)
-    cc.requests.get.assert_called_once_with(
+    curb_api('any_endpoint', headers)
+    requests_mock.get.assert_called_once_with(
       'http://challenge.shopcurbside.com/any_endpoint',
       headers = headers
     )
 
 class TestSessionGenerator:
-  def setup(self):
-    cc.curb_api = mock.Mock(side_effect = ['session1-10', 'session11-20', 'session21-30'])
-
   def setup_method(self, method):
-    self.sessions = cc.session_generator()
+    self.sessions = session_generator()
 
-  def test_get_session(self):
+  @mock.patch('curbside_challenge.curb_api')
+  def test_get_session(self, curb_api_mock):
     next(self.sessions)
-    cc.curb_api.assert_called_once_with('get-session')
+    curb_api_mock.assert_called_once_with('get-session')
 
-  def test_first_session(self):
+  @mock.patch('curbside_challenge.curb_api', side_effect = ['session1-10', 'session11-20', 'session21-30'])
+  def test_first_session(self, curb_api_mock):
     assert next(self.sessions) == 'session1-10'
 
-  def test_persist_session(self):
+  @mock.patch('curbside_challenge.curb_api', side_effect = ['session1-10', 'session11-20', 'session21-30'])
+  def test_persist_session(self, curb_api_mock):
     for i in range(10):
       assert next(self.sessions) == 'session1-10'
 
-  def test_get_new_session(self):
+  @mock.patch('curbside_challenge.curb_api', side_effect = ['session1-10', 'session11-20', 'session21-30'])
+  def test_get_new_session(self, curb_api_mock):
     for i in range(10):
       next(self.sessions)
     assert next(self.sessions) == 'session11-20'
 
-  def test_get_many_sessions(self):
+  @mock.patch('curbside_challenge.curb_api', side_effect = ['session1-10', 'session11-20', 'session21-30'])
+  def test_get_many_sessions(self, curb_api_mock):
     for i in range(10):
       assert next(self.sessions) == 'session1-10'
     for i in range(10):
@@ -77,23 +85,24 @@ class TestSessionGenerator:
       assert next(self.sessions) == 'session21-30'
 
 class TestGetResponse:
-  def setup(self):
-    self.example_response = '{"Any": "Response"}'
-    self.example_result = {'any': 'Response'}
-    cc.curb_api = mock.Mock(return_value = self.example_response)
-    cc.sessions = mock.Mock()
-    cc.sessions.__next__ = mock.Mock(return_value = 'session_id')
+  @mock.patch('curbside_challenge.sessions')
+  @mock.patch('curbside_challenge.curb_api', return_value = '{"Any": "Response"}')
+  def test_makes_curb_api_request(self, curb_api_mock, sessions_mock):
+    sessions_mock.__next__ = mock.Mock(return_value = 'session_id')
+    get_response('endpoint_with_session')
+    curb_api_mock.assert_called_with('endpoint_with_session', {'session': 'session_id'})
 
-  def test_makes_curb_api_request(self):
-    cc.get_response('endpoint_with_session')
-    cc.curb_api.assert_called_with('endpoint_with_session', {'session': 'session_id'})
+  @mock.patch('curbside_challenge.sessions')
+  @mock.patch('curbside_challenge.curb_api', return_value = '{"Any": "Response"}')
+  def test_dict_response(self, curb_api_mock, sessions_mock):
+    sessions_mock.__next__ = mock.Mock(return_value = 'session_id')
+    assert type(get_response('endpoint_with_session')) == dict
 
-  def test_dict_response(self):
-    assert type(cc.get_response('endpoint_with_session')) == dict
-
-  def test_returned_value(self):
-    assert cc.get_response('endpoint_with_session') == self.example_result
-
+  @mock.patch('curbside_challenge.sessions')
+  @mock.patch('curbside_challenge.curb_api', return_value = '{"Any": "Response"}')
+  def test_returned_value(self, curb_api_mock, sessions_mock):
+    sessions_mock.__next__ = mock.Mock(return_value = 'session_id')
+    assert get_response('endpoint_with_session') == {'any': 'Response'}
 
 @pytest.mark.parametrize("api_response,expected", [
   (
@@ -120,6 +129,5 @@ class TestGetResponse:
   ),
 ])
 def test_get_secret(api_response, expected):
-  print(api_response)
-  cc.get_response = mock.Mock(side_effect = api_response)
-  assert cc.get_secret('any_endpoint') == expected
+  with mock.patch('curbside_challenge.get_response', side_effect = api_response) as get_response_mock:
+    assert get_secret('any_endpoint') == expected
